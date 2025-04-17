f52244f22ab919c0d1b6ce39d4befda60fc55353
Willi-anD <willian270597@edu.unipar.br>
2025-04-17 20:15:48 -0300

    Primeiro Commit 17/04/2025

---
+++ src/Main.kt
@@ -0,0 +1,80 @@
+fun main() {
+    val bubalsauro = mapOf(//1
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA", "GRAMA"))
+
+    val ivysaur = mapOf(//2
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA", "GRAMA"))
+
+    val venusaur = mapOf(//3
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA", "GRAMA"))
+
+    val charmander = mapOf(//4
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val charmeleon = mapOf(//5
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val charizard = mapOf(//6
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val squirtle = mapOf(//7
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val wartortle = mapOf(//8
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val blastoise = mapOf(//9
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val caterpie = mapOf(//10
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"))
+
+    val metapod = mapOf(//11
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"))
+
+    val butterfree = mapOf(//12
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val weedle = mapOf(//13
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "VENENO" to listOf("FADA", "GRAMA"))
+
+    val kakuna = mapOf(//14
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "VENENO" to listOf("FADA", "GRAMA"))
+
+    val beedrill = mapOf(//15
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "VENENO" to listOf("FADA", "GRAMA"))
+
+    val pidgey = mapOf(//16
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val pidgeotto = mapOf(//17
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val pidgeot = mapOf(//18
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val rattata = mapOf(//19
+        "NORMAL" to listOf(""))
+
+    val raticate = mapOf(//20
+        "NORMAL" to listOf(""))
+
+    val spearow = mapOf(//21
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val fearow = mapOf(//22
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+}
