6b22f3d7ff3efb2f31145179ab98f1c2833dc8a3
Emer Marchi <marchimeson123@gmail.com>
2025-04-23 16:26:43 -0300

    Ultimo Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,15 @@
 fun main() {
     val bubalsauro = mapOf(//1
         "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
-        "VENENO" to  listOf("FADA", "GRAMA"))
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
 
     val ivysaur = mapOf(//2
         "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
-        "VENENO" to  listOf("FADA", "GRAMA"))
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
 
     val venusaur = mapOf(//3
         "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
-        "VENENO" to  listOf("FADA", "GRAMA"))
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
 
     val charmander = mapOf(//4
         "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
@@ -72,7 +72,6 @@ fun main() {
 
     val spearow = mapOf(//21
         "NORMAL" to listOf(""),
-        "NORMAL" to listOf(""),
         "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
 
     val fearow = mapOf(//22
@@ -98,7 +97,7 @@ fun main() {
         "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"))
 
     val nidoranFeminino = mapOf(//29
-    "VENENO" to  listOf("FADA", "GRAMA"))
+        "VENENO" to  listOf("FADA", "GRAMA"))
 
     val nidorina = mapOf(//30
         "VENENO" to  listOf("FADA", "GRAMA"))
@@ -122,4 +121,401 @@ fun main() {
 
     val clefable = mapOf(//36
         "FADA" to listOf("COMBATE", "DRAGÃO", "ESCURO"))
+
+    val vulpix = mapOf(//37
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val ninetales = mapOf(//38
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val jigglypuff = mapOf(//39
+        "NORMAL" to listOf(""),
+        "FADA" to listOf("COMBATE", "DRAGÃO", "ESCURO"))
+
+    val wigglytuff = mapOf(//40
+        "NORMAL" to listOf(""),
+        "FADA" to listOf("COMBATE", "DRAGÃO", "ESCURO"))
+
+    val zubat =  mapOf(//41
+        "TÓXICO" to  listOf("FADA", "GRAMA"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val golbat = mapOf(//42
+        "TÓXICO" to  listOf("FADA", "GRAMA"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val estranho = mapOf(//43
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val melancolia = mapOf(//44
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val vileplume =  mapOf(//45
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val paras = mapOf(//46
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"))
+
+    val parasect = mapOf(//47
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"))
+
+    val venonat = mapOf(//48
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"))
+
+    val venomoth = mapOf(//49
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"))
+
+    val diglett =  mapOf(//50
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"))
+
+    val dugtrio = mapOf(//51
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"))
+
+    val miau = mapOf(//52
+        "NORMAL" to listOf(""))
+
+    val persa = mapOf(//53
+        "NORMAL" to listOf(""))
+
+    val pysduck = mapOf(//54
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val golduck = mapOf(//55
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val mankey = mapOf(//56
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val primeape = mapOf(//57
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val growlithe = mapOf(//58
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val arcanine = mapOf(//59
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val poliwag = mapOf(//60
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val poliwhirl = mapOf(//61
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val poliwrath = mapOf(//62
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val abra = mapOf(//63
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val kadabra = mapOf(//64
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val alakazam = mapOf(//65
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val machop = mapOf(//66
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val machoke = mapOf(//67
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val machamp = mapOf(//68
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val bellsprout = mapOf(//69
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val weepinbell = mapOf(//70
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val victreebel = mapOf(//71
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val tentacool = mapOf(//72
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val tentacruel = mapOf(//73
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val geodude = mapOf(//74
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val graveler = mapOf(//75
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val golem = mapOf(//76
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val ponyta = mapOf(//77
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val rapidash = mapOf(//78
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val slowpoke = mapOf(//79
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val slowbro = mapOf(//80
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val magnemite = mapOf(//81
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"),
+        "AÇO" to listOf("PEDRA", "GELO", "FADA"))
+
+    val magneton = mapOf(//82
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"),
+        "AÇO" to listOf("PEDRA", "GELO", "FADA"))
+
+    val farfetchd = mapOf(//83
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val doduo = mapOf(//84
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val dodrio = mapOf(//85
+        "NORMAL" to listOf(""),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val seel = mapOf(//86
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val dewgong = mapOf(//87
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "GELO" to listOf("VÔO", "CHÃO", "GRAMA", "DRAGÃO"))
+
+    val grimer = mapOf(//88
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val muk = mapOf(//89
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val shellder = mapOf(//90
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val cloyster = mapOf(//91
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "GELO" to listOf("VÔO", "CHÃO", "GRAMA", "DRAGÃO"))
+
+    val gastly = mapOf(//92
+        "FANTASMA" to listOf("FANTASMA", "PSÍQUICO"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val haunter = mapOf(//93
+        "FANTASMA" to listOf("FANTASMA", "PSÍQUICO"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val gengar = mapOf(//94
+        "FANTASMA" to listOf("FANTASMA", "PSÍQUICO"),
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val onix = mapOf(//95
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val drowzee = mapOf(//96
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val hypno = mapOf(//97
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val krabby = mapOf(//98
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val kingler = mapOf(//99
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val voltorb = mapOf(//100
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"))
+
+    val electrode = mapOf(//101
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"))
+
+    val exeggcute =  mapOf(//102
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val exeggutor = mapOf(//103
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val cubone = mapOf(//104
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"))
+
+    val marowak = mapOf(//105
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"))
+
+    val hitmonlee = mapOf(//106
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val hitmonchan = mapOf(//107
+        "COMBATE" to listOf("NORMAL", "GELO", "PEDRA", "AÇO", "ESCURO"))
+
+    val lickitung = mapOf(//108
+        "NORMAL" to listOf(""))
+
+    val koffing = mapOf(//109
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val weezing = mapOf(//110
+        "TÓXICO" to  listOf("FADA", "GRAMA"))
+
+    val rhyhorn = mapOf(//111
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val rhydon = mapOf(//112
+        "CHÃO" to listOf("TÓXICO", "PEDRA", "FOGO", "ELÉTRICO", "AÇO"),
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"))
+
+    val chansey = mapOf(//113
+        "NORMAL" to listOf(""))
+
+    val tangela = mapOf(//114
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"))
+
+    val kangaskhan =  mapOf(//115
+        "NORMAL" to listOf(""))
+
+    val horsea = mapOf(//116
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val seadra = mapOf(//117
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val goldeen = mapOf(//118
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val seajing = mapOf(//119
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val staryu = mapOf(//120
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val starmie = mapOf(//121
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val mrmime = mapOf(//122
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"),
+        "FADA" to listOf("COMBATE", "DRAGÃO", "ESCURO"))
+
+    val scyther = mapOf(//123
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val jynx = mapOf(//124
+        "GELO" to listOf("VÔO", "CHÃO", "GRAMA", "DRAGÃO"),
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val electabuzz = mapOf(//125
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"))
+
+    val magmar = mapOf(//126
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val pinsir = mapOf(//127
+        "ERRO" to listOf("GRAMA", "PSÍQUICO", "ESCURO"))
+
+    val tauros = mapOf(//128
+        "NORMAL" to listOf(""))
+
+    val magikarp = mapOf(//129
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val gyarados = mapOf(//130
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val lapras = mapOf(//131
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"),
+        "GELO" to listOf("VÔO", "CHÃO", "GRAMA", "DRAGÃO"))
+
+    val ditto = mapOf(//132
+        "NORMAL" to listOf(""))
+
+    val eevee = mapOf(//133
+        "NORMAL" to listOf(""))
+
+    val vaporeon = mapOf(//134
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val jolteon = mapOf(//135
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"))
+
+    val flareon = mapOf(//136
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"))
+
+    val porygon = mapOf(//137
+        "NORMAL" to listOf(""))
+
+    val omanyte = mapOf(//138
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"),
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val omastar = mapOf(//139
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"),
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val kabuto = mapOf(//140
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"),
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val kabutops = mapOf(//141
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"),
+        "ÁGUA" to listOf("CHÃO", "PEDRA", "FOGO"))
+
+    val aerodactyl = mapOf(//142
+        "PEDRA" to  listOf("VÔO", "ERRO", "FOGO", "GELO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val snorlax = mapOf(//143
+        "NORMAL" to listOf(""))
+
+    val articuno =mapOf(//144
+        "GELO" to listOf("VÔO", "CHÃO", "GRAMA", "DRAGÃO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val zapdos = mapOf(//145
+        "ELÉTRICO" to listOf("VÔO", "ÁGUA"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val moltres = mapOf(//146
+        "FOGO" to listOf("ERRO", "PLANTA", "GELO", "AÇO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val dratini = mapOf(//147
+        "DRAGÃO" to listOf("DRAGÃO"))
+
+    val dragonair = mapOf(//148
+        "DRAGÃO" to listOf("DRAGÃO"))
+
+    val dragonite = mapOf(//149
+        "DRAGÃO" to listOf("DRAGÃO"),
+        "VÔO" to listOf("COMBATE", "ERRO", "GRAMA"))
+
+    val mewtwo = mapOf(//150
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
+
+    val mew = mapOf(//151
+        "PSÍQUICO" to listOf("COMBATE", "TÓXICO"))
 }
